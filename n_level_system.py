2276d677fbd719ee05985b3bf205ba3bb74e2cf5
Alex_Hackett <ahackett@tcd.ie>
2018-07-10 14:07:20 +0100

    yeet

--- n_level_system.py
+++ n_level_system.py
@@ -26,11 +26,12 @@ import emailer as email
 hbar = 6.582119514e-16
 kb = 8.6173303e-5
 m0 = 9.10938356e-31
+u = 4810
 #--------------------------------------------------
 num_sites = 100
-num_steps = 100
+num_steps = 1000
 temp = 20
-k_values = np.linspace(0,100000,10)
+k_values = np.linspace(0,100000,100)
 
 class nLevelKMC:
     def __init__(self,num_sites,num_steps,temp,k_values):
@@ -38,40 +39,43 @@ class nLevelKMC:
         self.num_steps = num_steps
         self.temp = temp
         self.k_values = k_values
-        self.sites = np.zeros(self.num_sites)
+        self.sites = np.zeros(self.num_sites, dtype = int)
         self.rate_lookup = np.zeros((len(self.k_values),len(self.k_values)), dtype = float)
         self.rates = np.zeros((len(self.k_values),len(self.k_values)), dtype = float)
         self.state_values = np.zeros(len(k_values), dtype = float)
         self.rate_sum = 0
         self.chosen_transition = 0
         self.time = 0
-        self.time_array = []
+        self.time_array = [0]
         self.state_array = []
+        self.total_momentum = []
         
     def initializeStates(self, state = 'gs'):
         if state == 'gs':
-            for i in range(self.sites):
+            for i in range(len(self.sites)):
                 self.sites[i] = 0
         elif state == 'nonRes':
-            for i in range(self.sites):
-                self.sites[i] = 99
+            for i in range(len(self.sites)):
+                self.sites[i] = len(k_values) - 1
         elif state == 'random':
-            for i in range(self.sites):
-                self.sites[i] = int(np.floor(100*random()))
+            for i in range(len(self.sites)):
+                self.sites[i] = int(np.floor(len(k_values)*random()))
                 
     def stateCounter(self):
         for i in range(len(self.k_values)):
             self.state_values[i] = (self.sites == i).sum()
                 
     def makeRateLookup(self, decay = 0):
-        for i in len(self.k_values):
-            for j in len(self.k_values):
+        for i in range(len(self.k_values)):
+            for j in range(len(self.k_values)):
                 self.rate_lookup[i,j]= psp.W_k_k_prime(self.k_values[i], self.k_values[j], self.temp)
     
     def updateRates(self, decay = 0):
-        for i in len(self.k_values):
-            for j in len(self.k_values):
+        for i in range(len(self.k_values)):
+            for j in range(len(self.k_values)):
                 self.rates[i,j] = self.rate_lookup[i,j] * self.state_values[i] * (1 + self.state_values[j])
+                if np.isnan(self.rates[i,j]):
+                    self.rates[i,j] = 0
                 
     def selectEvent(self):
         self.rate_sum = self.rates.sum()
@@ -87,7 +91,8 @@ class nLevelKMC:
         self.chosen_transition = [int(site/len(k_values)), int(site%len(k_values))]
         
     def executeEvent(self):
-        valid_sites =  self.sites.searchsorted(self.chosen_transition[0])
+        #valid_sites =  [self.sites.searchsorted(self.chosen_transition[0])]
+        valid_sites = np.nonzero(np.in1d(self.sites,self.chosen_transition[0]))[0]
         chosen_site = choice(valid_sites)
         self.sites[chosen_site] = self.chosen_transition[1]
         
@@ -99,21 +104,33 @@ class nLevelKMC:
         self.time_array.append(self.time)
         
     def trackConfig(self):
-        self.state_array.append(self.sites)
-        
+        self.state_array.append(np.array(self.sites))
+        self.total_momentum.append(11111.111111111111 * np.array(self.sites.sum()))
     
 kmc = nLevelKMC(num_sites,num_steps,temp,k_values)
 kmc.initializeStates()
 kmc.makeRateLookup()
 kmc.stateCounter()
 kmc.trackConfig()
-for i in range(len(kmc.num_steps)):
+
+for i in range(kmc.num_steps):
     kmc.updateRates()
     kmc.selectEvent()
     kmc.executeEvent()
     kmc.advanceTime()
     kmc.stateCounter()
     kmc.trackConfig()
+    
+e_loss = hbar*(u * kmc.total_momentum[-1])
+ans = str('Total Phonon Energy Absorbed = '+ str(e_loss) +' eV')
+print(ans)
+fig1 = plt.figure()
+plt.plot(kmc.time_array,kmc.total_momentum)
+plt.title('Total Polariton Momentum of System wrt Time')
+plt.xlabel('Time (Seconds)')
+plt.ylabel(r'Total Momentum ($m^{-1}$)')
+#txt = plt.text(kmc.time_array[int(len(kmc.time_array)/5)], kmc.total_momentum[int(len(kmc.total_momentum)/5)],ans)
+plt.show()
 
         
 
