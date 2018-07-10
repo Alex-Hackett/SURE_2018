b3ab13f4a76932e387d9774b659f504100a8481c
Alex_Hackett <ahackett@tcd.ie>
2018-07-10 13:07:35 +0100

    dsa

--- n_level_system.py
+++ n_level_system.py
@@ -42,6 +42,11 @@ class nLevelKMC:
         self.rate_lookup = np.zeros((len(self.k_values),len(self.k_values)), dtype = float)
         self.rates = np.zeros((len(self.k_values),len(self.k_values)), dtype = float)
         self.state_values = np.zeros(len(k_values), dtype = float)
+        self.rate_sum = 0
+        self.chosen_transition = 0
+        self.time = 0
+        self.time_array = []
+        self.state_array = []
         
     def initializeStates(self, state = 'gs'):
         if state == 'gs':
@@ -66,8 +71,50 @@ class nLevelKMC:
     def updateRates(self, decay = 0):
         for i in len(self.k_values):
             for j in len(self.k_values):
+                self.rates[i,j] = self.rate_lookup[i,j] * self.state_values[i] * (1 + self.state_values[j])
+                
+    def selectEvent(self):
+        self.rate_sum = self.rates.sum()
+        rand_rate = 0
+        while rand_rate == 0:
+            rand_rate = random()
+        site = 0
+        cumulative_rate = self.rates.flat[site]
+        while cumulative_rate < (self.rate_sum * rand_rate):
+            cumulative_rate += self.rates.flat[site + 1]
+            site += 1
+        self.chosen_transition = site
+        self.chosen_transition = [int(site/len(k_values)), int(site%len(k_values))]
+        
+    def executeEvent(self):
+        valid_sites =  self.sites.searchsorted(self.chosen_transition[0])
+        chosen_site = choice(valid_sites)
+        self.sites[chosen_site] = self.chosen_transition[1]
+        
+    def advanceTime(self):
+        dt = 0
+        rand_time = random()
+        dt = -np.log(rand_time) / (self.rate_sum)
+        self.time += dt
+        self.time_array.append(self.time)
+        
+    def trackConfig(self):
+        self.state_array.append(self.sites)
+        
+    
+kmc = nLevelKMC(num_sites,num_steps,temp,k_values)
+kmc.initializeStates()
+kmc.makeRateLookup()
+kmc.stateCounter()
+kmc.trackConfig()
+for i in range(len(kmc.num_steps)):
+    kmc.updateRates()
+    kmc.selectEvent()
+    kmc.executeEvent()
+    kmc.advanceTime()
+    kmc.stateCounter()
+    kmc.trackConfig()
 
-            
         
 
 
